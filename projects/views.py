de4c41dd7484f4889926372d9aa55c5ced9962e9
HoJin9622 <kiss0104040@gmail.com>
2022-11-14 16:19:35 +0900

    ✨ feat: 텍스트 전처리기 생성

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,24 @@
+import re
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import ParseError
+
+
+def text_preprocessor(text):
+    separated_text = re.split("[.!?]", text)
+    no_gap_text = []
+    for text in separated_text:
+        if text.isspace():
+            continue
+        no_gap_text.append(text.strip())
+    return no_gap_text
 
 
 class Projects(APIView):
-    pass
+    def post(self, request):
+        text = request.data.get("text")
+        if not text:
+            raise ParseError("text는 필수입니다.")
+        new_text = text_preprocessor(text)
+        print(new_text)
+        return Response()
