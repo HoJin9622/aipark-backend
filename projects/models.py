d8bff47019fa5a7be22d608750b2d5a079f92efb
HoJin9622 <kiss0104040@gmail.com>
2022-11-14 15:31:18 +0900

    ✨ feat: 오디오 모델 생성

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from common.models import CommonModel
 
 
+class Audio(CommonModel):
+    index = models.PositiveIntegerField(verbose_name="순서")
+    text = models.CharField(max_length=80, verbose_name="오디오로 변환된 텍스트")
+    speed = models.PositiveIntegerField(verbose_name="오디오의 스피드")
+    project = models.ForeignKey(
+        "Project",
+        on_delete=models.CASCADE,
+        related_name="projects",
+        verbose_name="프로젝트",
+    )
+
+    class Meta:
+        verbose_name = "오디오"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.text
+
+
 class Project(CommonModel):
     index = models.PositiveIntegerField(verbose_name="순서")
     project_id = models.PositiveIntegerField(verbose_name="프로젝트 ID")
